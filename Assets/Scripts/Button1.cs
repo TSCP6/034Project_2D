a9bd51c13b3fbb8b0dd1e4f1ff6d533cb65bb347
your name <your.email@example.com>
2026-03-07 11:38:28 +0800

    我做了一个简单的按钮和ui

---
+++ Assets/Scripts/Button1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button1 : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        Debug.Log("按钮被点击了！");
+        // 你可以在这里写下你想要的功能，比如切换场景
+    }
+}
